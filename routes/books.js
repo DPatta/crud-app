a401e215f23609e3908b3f5e4e6769b0b361b871
supakit kitjanabumrungsak <supakit.kit@thailife.com>
2022-06-05 02:38:58 +0700

    change connection db

--- routes/books.js
+++ routes/books.js
@@ -1,142 +1,159 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let dbCon = require('../lib/db');
+let conn = require("../lib/db");
 
-router.get('/', (req, res, next ) => {
-    dbCon.query('SELECT * FROM books ORDER BY id ', (err, rows)=> {
-        if (err) {
-            req.flash('error', err);
-            res.render('books', {data:''});
-        } else {
-            res.render('books', {data:rows});
-        }
-    })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    conn.query("SELECT * FROM books ORDER BY id", (err, { rows }) => {
+      if (err) throw err;
+      res.render("books", { data: rows });
+      //   conn.end();
+    });
+  } catch (err) {
+    req.flash("error", err);
+    res.render("books", { data: "" });
+  }
+});
 
 // display add book page
-router.get('/add', (req,res,next) => {
-    res.render('books/add',{
-        name: '',
-        author:''
-    })
-})
+router.get("/add", (req, res, next) => {
+  res.render("books/add", {
+    name: "",
+    author: "",
+  });
+});
 
-//ADD new book 
-router.post('/add', (req,res,next) => {
-    let name = req.body.name;
-    let author = req.body.author;
-    let errors = false;
+//ADD new book
+router.post("/add", (req, res, next) => {
+  let name = req.body.name;
+  let author = req.body.author;
+  let errors = false;
 
-    if (name.length === 0 || author.length === 0) {
-        errors = true;
-        // set flash messages
-        req.flash('error', 'Please enter name and author');
-        //red to add.jad with flash message
-        res.render('books/add', {
-            name: name,
-            author: author
-        })
+  if (name.length === 0 || author.length === 0) {
+    errors = true;
+    // set flash messages
+    req.flash("error", "Please enter name and author");
+    //red to add.jad with flash message
+    res.render("books/add", {
+      name: name,
+      author: author,
+    });
+  }
+  //no error
 
-    }
-    //no error
+  if (!errors) {
+    let form_data = {
+      name: name,
+      author: author,
+    };
 
-    if(!errors){
-        let form_data ={
-            name: name,
-            author: author
+    //insert
+
+    try {
+      conn.query(
+        "INSERT INTO books (name, author) VALUES ($1, $2)",
+        [form_data.name, form_data.author],
+        (err, _rs) => {
+          if (err) throw err;
+          req.flash("success", "Book successfully added");
+          res.redirect("/books");
+          //   conn.end();
         }
-
-        //insert
-        dbCon.query('INSERT INTO books SET ?', form_data, (err, result) =>{
-            if(err) {
-                req.flash('error',err)
-
-                res.render('books/add', {
-                    name: form_data.name,
-                    author: form_data.author
-                })
-            } else {
-                req.flash('success', 'Book successfully added');
-                res.redirect('/books');
-            }
-        })
-
+      );
+    } catch (err) {
+      req.flash("error", err);
+      res.render("books/add", {
+        name: form_data.name,
+        author: form_data.author,
+      });
     }
-})
+  }
+});
 
 // edit book page
-router.get('/edit/(:id)', (req,res,next)=> {
-    let id = req.params.id;
+router.get("/edit/(:id)", (req, res, next) => {
+  let id = req.params.id;
 
-    dbCon.query('SELECT * FROM books WHERE id =' + id, (err,rows,fields) =>{
-        if (rows.length <= 0) {
-            req.flash('error','Book not found with id = ' + id)
-            res.redirect('/books');
-        } else {
-            res.render('books/edit',{
-                title: 'Edit book',
-                id: rows[0].id,
-                name:rows[0].name,
-                author:rows[0].author
-            })
-        }
-    })
-})
+  try {
+    conn.query("SELECT * FROM books WHERE id = $1", [id], (err, { rows }) => {
+      if (rows.length == 0) {
+        req.flash("error", "Book not found with id = " + id);
+        res.redirect("/books");
+      } else {
+        res.render("books/edit", {
+          title: "Edit book",
+          id: rows[0].id,
+          name: rows[0].name,
+          author: rows[0].author,
+        });
+      }
+      //   conn.end();
+    });
+  } catch (err) {
+    req.flash("error", err);
+    res.render("/books", {
+      name: form_data.name,
+      author: form_data.author,
+    });
+  }
+});
 
 //update book
-router.post('/update/:id', (req,res,next) =>{
-    let id = req.params.id;
-    let name = req.body.name;
-    let author = req.body.author;
-    let errors = false;
+router.post("/update/:id", (req, res, next) => {
+  let id = req.params.id;
+  let name = req.body.name;
+  let author = req.body.author;
+  let errors = false;
 
-    if (name.length === 0 || author.length ===0 ) {
-        errors =true;
-        req.flash('error', 'Please enter name and author');
-        res.render('books/edit', {
-            id: req.params.id,
-            name: name,
-            author: author
-
-        })
-    }
-    //no error
-    if (!errors) {
-        let form_data = {
-            name: name,
-            author:author
+  if (name.length === 0 || author.length === 0) {
+    errors = true;
+    req.flash("error", "Please enter name and author");
+    res.render("books/edit", {
+      id: req.params.id,
+      name: name,
+      author: author,
+    });
+  }
+  //no error
+  if (!errors) {
+    let form_data = {
+      name: name,
+      author: author,
+    };
+    //update query
+    try {
+      conn.query(
+        "UPDATE books SET name=$1, author=$2 WHERE id =$3",
+        [form_data.name, form_data.author, id],
+        (err, _rs) => {
+          if (err) throw err;
+          req.flash("success", "Book successfully updated");
+          res.redirect("/books");
         }
-        //update query
-        dbCon.query("UPDATE books SET ? WHERE id =" + id, form_data,(err, result) =>{
-            if(err){
-                req.flash('error', err);
-                res.render('books/edit',{
-                    id: req.params.id,
-                    name: form_data.name,
-                    author: form_data.author
-                })
-
-            } else {
-                req.flash('success', 'Book successfully updated');
-                res.redirect('/books')
-            }
-        })
+      );
+    } catch (err) {
+      req.flash("error", err);
+      res.render("books/edit", {
+        id: req.params.id,
+        name: form_data.name,
+        author: form_data.author,
+      });
     }
+  }
+});
 
-})
-
-//delete book 
-router.get('/delete/(:id)', (req,res,next) => {
-    let id = req.params.id;
-
-    dbCon.query('DELETE FROM books WHERE id =' + id, (err,result)=>{
-        if(err) {
-            req.flash('error', err)
-            res.redirect('/books');
-        } else {
-            req.flash('success', 'Bookssuccessfully deleted! ID =' + id);
-            res.redirect('/books');
-        }
-    })
-})
+//delete book
+router.get("/delete/(:id)", (req, res, next) => {
+  let id = req.params.id;
+  try {
+    conn.query("DELETE FROM books WHERE id =$1", [id], (err, _rs) => {
+      if (err) throw err;
+      req.flash("success", "Bookssuccessfully deleted! ID =" + id);
+      res.redirect("/books");
+    });
+  } catch (err) {
+    req.flash("error", err);
+    res.redirect("/books");
+  }
+});
 module.exports = router;
